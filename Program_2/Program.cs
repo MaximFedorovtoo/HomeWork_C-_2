3484f70663f14fbc8b1221c58f86afe30ea89d3a
MaximFedorovtoo <makfedorovtooo@gmail.com>
2022-12-03 01:09:35 +0300

    HomeWork done

---
+++ Program_2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число чтобы узнать его третью цифру");
+int number = int.Parse(Console.ReadLine());
+int count = 0;
+if( number>1000 || number >=100 )
+{
+    while (number > 1000) 
+    {
+        number = number/10;
+    }
+    int third_numb = number%10;
+    Console.WriteLine($"Третья цифра: {third_numb}");
+}
+else{Console.WriteLine("Третьей цифры нет");}
---
+++ Program_3/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число обозначающее день недели, проверим является ли этот день выходным?");
+int numberDay = int.Parse(Console.ReadLine());
+void WeekDay(int var)
+{
+    if(var == 6 || var == 7){Console.WriteLine("Да это выходной!");}
+    else{Console.WriteLine("Нет это не выходной!");}
+}
+WeekDay(numberDay);
