ecab6f19f4417e2fd037401bee4be6abc861ecce
MaximFedorovtoo <makfedorovtooo@gmail.com>
2022-12-03 01:37:44 +0300

    All done

--- Program_3/Program.cs
+++ Program_3/Program.cs
@@ -1,9 +1,14 @@
-﻿Console.Clear();
-Console.WriteLine("Введите число обозначающее день недели, проверим является ли этот день выходным?");
-int numberDay = int.Parse(Console.ReadLine());
+﻿//Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+//6 -> да
+//7 -> да
+//1 -> нет
+
+Console.Clear();
 void WeekDay(int var)
 {
     if(var == 6 || var == 7){Console.WriteLine("Да это выходной!");}
     else{Console.WriteLine("Нет это не выходной!");}
 }
+Console.WriteLine("Введите число обозначающее день недели, проверим является ли этот день выходным?");
+int numberDay = int.Parse(Console.ReadLine());
 WeekDay(numberDay);
